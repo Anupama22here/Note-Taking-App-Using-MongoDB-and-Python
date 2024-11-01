8e948738985e2d65fe44d0daa8585ab9e9dabf62
Anupama <anupama0223@gmail.com>
2024-11-01 18:16:06 +0530

    initial commit

---
+++ app/pages/About_this_project.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("About this project")
+
+st.write()
+
+st.markdown("""
+The main goal of this project was to learn how to create and manage a mongodb database and its collections, while also practicing creating secure authentication login process.
+
+I chose streamlit as a framework because of its versatility and easy to create interface components/widgets.
+
+
+**Kanban dashboard**
+
+- use python and streamlit to create and vizualise the dashboard
+- use mongodb to store all the data from the tasks
+
+**Users login and signup interface**
+
+- using mongodb to store all data from client
+- clients can only see their tasks
+- create a secure authentication process
+
+""")
+
